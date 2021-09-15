13f514d6606914a54941e42dd2a596faf5760b3e
Aakash Anil Kolekar <aakashkolekar@email.arizona.edu>
2021-09-15 10:41:12 -0700

    exploring socket methods

--- simple_client_http1.0.py
+++ simple_client_http1.0.py
@@ -1,10 +1,10 @@
 from socket import *
 
 s = socket(AF_INET, SOCK_STREAM)
-s.connect(("www.ucsd.edu", 80))
-s.send(b"GET / HTTP/1.0\r\n\r\n")
-# s.connect(("www.u.arizona.edu", 80))
-# s.send(b"GET /~weichen/MIS543/test.html HTTP/1.0\r\n\r\n")
+# s.connect(("www.ucsd.edu", 80))
+# s.send(b"GET / HTTP/1.0\r\n\r\n")
+s.connect(("www.u.arizona.edu", 80))
+s.send(b"GET /~weichen/MIS543/test.html HTTP/1.0\r\n\r\n")
 data = s.recv(1024)
-print(data)
+print(data.decode())
 s.close()
